eb9220c87d541afb4e105ea6fc1ef6a9792d2e8c
Jaemyung Lee <lee10738@naver.com>
2022-05-19 16:05:55 +0900

    <공통단> mybatis-config.xml 에 경로 2개 추가 <개인단> 펀딩 준비 페이지 데이터 조회작성중

--- src/main/java/kr/or/fund/model/service/FundTmpService.java
+++ src/main/java/kr/or/fund/model/service/FundTmpService.java
@@ -1,10 +1,13 @@
 package kr.or.fund.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.or.fund.model.dao.FundTmpDao;
 import kr.or.fund.model.vo.TmpFund;
+import kr.or.fund.model.vo.TmpReward;
 
 @Service
 public class FundTmpService {
@@ -14,6 +17,16 @@ public class FundTmpService {
 	public int createTmpFund(TmpFund f) {
 		// TODO Auto-generated method stub
 		return dao.createTmpFund(f);
+	}
+
+	public TmpFund selectOneTmpFund(TmpFund tf) {
+		// TODO Auto-generated method stub
+		return dao.selectOneTmpFund(tf);
+	}
+
+	public ArrayList<TmpReward> selectReward(TmpFund tf) {
+		// TODO Auto-generated method stub
+		return dao.selectReward(tf);
 	}
 
 }
