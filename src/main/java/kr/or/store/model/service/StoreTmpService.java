84c726132346495b985d60395c844534ad5cfc96
suin Yeo <immof@naver.com>
2022-05-26 00:47:08 +0900

    store

--- src/main/java/kr/or/store/model/service/StoreTmpService.java
+++ src/main/java/kr/or/store/model/service/StoreTmpService.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.or.store.model.dao.StoreTmpDao;
+import kr.or.store.model.vo.TmpStore;
 
 @Service
 public class StoreTmpService {
 	@Autowired
 	private StoreTmpDao dao;
+
+	public int createTmpStore(TmpStore t) {
+		// TODO Auto-generated method stub
+		return dao.createTmpStore(t);
+	}
 }
