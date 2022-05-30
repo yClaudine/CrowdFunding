e18edb0d52c8e16aba39081099682d4e2e240c62
yClaudine <yamonism@gmail.com>
2022-05-30 15:24:07 +0900

    ysm - 상세 페이지 데이터 구현 - 좋아요, 신고 진행중

--- src/main/java/kr/or/fund/controller/FundListController.java
+++ src/main/java/kr/or/fund/controller/FundListController.java
@@ -1,6 +1,7 @@
 package kr.or.fund.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,8 +9,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
+
 import kr.or.fund.model.service.FundListService;
 import kr.or.fund.model.vo.Fund;
+import kr.or.fund.model.vo.FundLike;
+import kr.or.fund.model.vo.FundViewData;
+import kr.or.fund.model.vo.Reward;
 import kr.or.member.vo.Seller;
 
 @Controller
@@ -27,35 +33,79 @@ public class FundListController {
 		model.addAttribute("category",category);
 		return "fund/fundList";
 	}
-	//펀딩 리스트 메인 - 카테고리
-
-	
-	//펀딩 상세 메인페이지-story
+	/*펀딩 상세페이지
 	@RequestMapping(value="/fundView.do")
 	public String FundView(int fundNo, Model model)	{
-		Fund f = service.selectOneFund(fundNo);
+		Fund f = service.selectOneFundView(fundNo);
 		model.addAttribute("f",f);
 		return "fund/fundView";	
+	}*/
+	
+	//펀딩 상세 메인페이지-story 에러---------------------------------------------
+	@RequestMapping(value="/fundView.do")
+	public String FundView(int fundNo,Model model)	{
+		FundViewData fvd = service.selectOneFundView(fundNo);
+		model.addAttribute("s",fvd.getS());
+		model.addAttribute("f",fvd.getF());
+		model.addAttribute("list",fvd.getRewardList());
+		return "fund/fundView";	
 	}
-	//펀딩 상세2 - 반환정책
+
+	//펀딩 신고
+	@RequestMapping(value="/reportFund.do")
+	public String reportFund(int fundNo) {
+		int result = service.updateReportCount(fundNo);
+		System.out.println(result);
+		return "redirect:/fundView.do?fundNo="+fundNo;
+		//location.href="/fundView.do?fundNo=${f.fundNo }";
+	}
+	
+	//좋아요 ajax
+	@ResponseBody
+	@RequestMapping(value="/fundLike.do", produces="application/json;charset=utf-8")
+	public String fundLike(int fundNo, String memberId) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("fundNo",fundNo);
+		map.put("memberId",memberId);
+		FundLike fl = service.selectOneFundlike(map);
+		
+		if(fl==null) { //해당 펀딩에 좋아요 한 회원 없음  -> 좋아요 row update, 총 row수
+			int result = service.insertFundlike(/*FundLike f*/);
+			return "0";
+		}else{			//좋아요 한 회원 있음 -> 좋아요 row delete, 총 row수
+			int result = service.deleteFundlike(fundNo);
+			return "1";
+		}
+		//return new Gson().toJson();
+	}
+	
+	/*fundlike 하나 insert??
+	public String insertFundlike(FundLike flView) {
+		int result = service.insertFundlike(flView);
+		return "";
+	}*/
+	
+}
+
+
+
+
+	/*펀딩 상세2 - 반환정책
 	@RequestMapping(value="/fundViewReturnInfo.do")
 	public String FundViewReturnInfo(int fundNo, Model model) {
-		Fund f = service.selectOneFund(fundNo);
-		model.addAttribute("f",f);
+
 		return "fund/fundViewReturnInfo";	
 	}
 	//펀딩 상세3 - 새소식 게시판
 	@RequestMapping(value="/fundViewNotice.do")
 	public String FundViewNotice(int fundNo, Model model) {
-		Fund f = service.selectOneFund(fundNo);
-		model.addAttribute("f",f);
+
 		return "fund/fundViewNotice";	
 	}
 	//펀딩 상세4 - 서포터
 	@RequestMapping(value="/fundViewSupporter.do")
 	public String FundViewSupporter(int fundNo, Model model) {
-		Fund f = service.selectOneFund(fundNo);
-		model.addAttribute("f",f);
+
 		return "fund/fundViewSupporter";	
 	}
 	
@@ -69,4 +119,4 @@ public class FundListController {
 	}*/
 	
 	
-}
+
--- src/main/java/kr/or/fund/model/service/FundListService.java
+++ src/main/java/kr/or/fund/model/service/FundListService.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Service;
 
 import kr.or.fund.model.dao.FundListDao;
 import kr.or.fund.model.vo.Fund;
+import kr.or.fund.model.vo.FundLike;
+import kr.or.fund.model.vo.FundViewData;
+import kr.or.fund.model.vo.Reward;
+import kr.or.member.vo.Seller;
 
 @Service
 public class FundListService {
@@ -20,11 +24,38 @@ public class FundListService {
 		map.put("category", category);
 		return dao.selectFundList(map);
 	}
-	
-	//펀딩 상세 메인페이지-story
-	public Fund selectOneFund(int fundNo) {
+
+	/*
+	public Fund selectOneFundView(int fundNo) {
 		return dao.selectOneFund(fundNo);
+	}*/
+	
+	//펀딩 상세 페이지 데이터
+	public FundViewData selectOneFundView(int fundNo) {
+		Fund f = dao.selectOneFund(fundNo);
+		Seller s = dao.selectOneSeller(fundNo);
+		ArrayList<Reward> list = dao.selectRewardList(fundNo);
+		FundViewData fvd = new FundViewData(f,list,s);
+		return fvd;
 	}
 	
-	//펀딩 셀러 조회
+	
+
+	//펀딩 신고하기
+	public int updateReportCount(int fundNo) {
+		return dao.updateReportCount(fundNo);
+	}
+	//좋아요 ajax
+	public FundLike selectOneFundlike(HashMap<String, Object> map) {
+		return dao.selectOneFundlike(map);
+	}
+	//좋아요 insert
+	public int insertFundlike() {
+		return dao.insertFundlike();
+	}
+	//좋아요 delete
+	public int deleteFundlike(int fundNo) {
+		return dao.deleteFundlike(fundNo);
+	}
+
 }
